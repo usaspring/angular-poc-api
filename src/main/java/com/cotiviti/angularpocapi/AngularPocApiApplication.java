748fced939361af3827db07dff91b67887a90067
usaspring <usaspring@gmail.com>
2018-08-15 16:22:24 -0400

    init checking in

---
+++ src/main/java/com/cotiviti/angularpocapi/AngularPocApiApplication.java
@@ -0,0 +1,12 @@
+package com.cotiviti.angularpocapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularPocApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularPocApiApplication.class, args);
+	}
+}
